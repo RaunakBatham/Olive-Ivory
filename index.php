d38195267679b8414f12b835af02dcf4d89257ab
Raunak <007raunakbatham@gmail.com>
2019-02-16 21:08:33 +0530

    Added FontAwesome and HomePage Contents

--- index.php
+++ index.php
@@ -6,19 +6,24 @@
     <head>
     	<title>Olive Ivory</title>
         <link rel="stylesheet" href="home.css"/>
-    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="./nav.css">
+        <link rel="stylesheet" href="packages/fontawesome-free-5.7.2-web/css/all.css"/>
     </head>
     <body>
-<div class="topnav" id="myTopnav">
-  <a href="#home" class="active">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
-  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-  </a>
-</div>
-<script type="text/javascript src="navv.js"></script>
+        <div class="header">
+            <div class="contact">
+                <div class="info">
+                    <a href="tel:+91987654321" class="number"><i class="fas fa-phone"></i> +91 987654321</p>
+                    <a href="mailto:admin@hotel.com" class="email"><i class="far fa-envelope"></i> admin@hotel.com</p>
+                </div>
+                <div class="social">
+                    <a href="https://www.facebook.com"><i class="fab fa-facebook fa-lg"></i></a>
+                    <a href="https://www.google.com"><i class="fab fa-google-plus fa-lg"></i></a>
+                    <a href="https://www.twitter.com"><i class="fab fa-twitter fa-lg"></i></a>
+                </div>
+            </div>
+            <div class="navbar">
+            
+            </div>
+        </div>
     </body>
 </html>
