12e3cc4f4bb72959ccdb76ad63fde9f1b90b9a81
jashan11 <45364040+jashan11@users.noreply.github.com>
2019-02-19 17:11:44 +0530

    changes

--- index.php
+++ index.php
@@ -48,16 +48,14 @@ function scrollFunction() {
             <div id="navbar">
                 <table border="0" width="100%">
                     <tr><td align="right" width="47%">
-
-
     <div class="dropdown">
-  <button class="dropbtn"><a href="index.php">Hotel</a></button>
+  <button class="dropbtn"><a href="#hotel">Hotel</a></button>
   <div class="dropdown-content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
+    <a href="index.php">Link 1</a>
+    <a href="aboutus.html">Link 2</a>
+    <a href="history.html">Link 3</a>
   </div>
-
+</div>
 
      
     <a href="#contact">Rooms</a>
@@ -76,6 +74,22 @@ function scrollFunction() {
 </table>
 
 </div>
+
+
+<!--<div class="banner">
+    <h1>"Palava's beating Heart"</h1>
+    <p>is here</p>
+</div>-->
+
+
+
+
+
+
+<!--carousel Images-->
+
+
+
             
             <div class="carousel">
                 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
@@ -107,6 +121,35 @@ function scrollFunction() {
                 </div>
             </div>
         </div>
+
+
+<!-- WRAPPER FOR SLIDES -->
+                    <div class="carousel-inner">
+                        <div class="active item">
+                            <div class="carousel-content">
+                                <img src="img/Slide 1.jpg" alt="..." position="relative">
+                                <p>TEST</p>
+                            </div>
+
+                            <div class="carousel-caption">
+                                <h3>What we do</h3>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="http://placehold.it/1200x315" alt="...">
+                            <div class="carousel-caption">
+                                <h3>What we Do</h3>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="http://placehold.it/1200x315" alt="...">
+                            <div class="carousel-caption">
+                                <h3>Who we Are</h3>
+                            </div>
+                        </div>
+                    </div>
+
+        
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
