c383c5f0cbd2302bed73e4809a6e7f684b791602
jashan11 <45364040+jashan11@users.noreply.github.com>
2019-02-19 20:34:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -121,35 +121,6 @@ function scrollFunction() {
                 </div>
             </div>
         </div>
-
-
-<!-- WRAPPER FOR SLIDES -->
-                    <div class="carousel-inner">
-                        <div class="active item">
-                            <div class="carousel-content">
-                                <img src="img/Slide 1.jpg" alt="..." position="relative">
-                                <p>TEST</p>
-                            </div>
-
-                            <div class="carousel-caption">
-                                <h3>What we do</h3>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <img src="http://placehold.it/1200x315" alt="...">
-                            <div class="carousel-caption">
-                                <h3>What we Do</h3>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <img src="http://placehold.it/1200x315" alt="...">
-                            <div class="carousel-caption">
-                                <h3>Who we Are</h3>
-                            </div>
-                        </div>
-                    </div>
-
-        
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
