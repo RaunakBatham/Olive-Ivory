1aa1cdc4fc39aae9edaa5a154e6209afc5e15e8f
Raunak <007raunakbatham@gmail.com>
2019-02-21 22:00:00 +0530

    Added Login BackEnd

--- index.php
+++ index.php
@@ -3,35 +3,7 @@
     	<title>Olive Ivory</title>
         <link rel="stylesheet" href="packages/fontawesome-free-5.7.2-web/css/all.css"/>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<<<<<<< HEAD
-        <link rel="stylesheet" href="home.css"/>        
-=======
         <link rel="stylesheet" href="home.css"/>
-
-
-        <script type="text/javascript">
-            window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.padding = "5px 8px";
-    document.getElementById("navbar").style.top = "0px";
-    document.getElementById("navbar").style.background = "#000000";
-
-
-
-  } else {
-    document.getElementById("navbar").style.padding = "20px 10px";
-    document.getElementById("navbar").style.top = "25px";
-    document.getElementById("navbar").style.background ="transparent";
-
-
-  }
-}
-
-        </script>
-
->>>>>>> parent of 8b61fbf... Created index.js
     </head>
 
     <body>
@@ -40,23 +12,23 @@ function scrollFunction() {
                 <div class="info">
                     <a href="tel:+918360806758"><i class="fas fa-phone"></i> +91 8360806758</a>
                     <a href="mailto:jashan113114@gmail.com"><i class="far fa-envelope"></i> admin@hotel.com</a>
-                </div>http://127.0.0.1:8080/index.php#carouselExampleControls
+                </div>
                 <div class="social">
+                    <a href="https://www.facebook.com"><i class="fab fa-facebook fa-lg"></i></a>
+                    <a href="https://www.facebook.com"><i class="fab fa-facebook fa-lg"></i></a>
+                    <a href="https://www.facebook.com"><i class="fab fa-facebook fa-lg"></i></a>
+                    <a href="https://www.facebook.com"><i class="fab fa-facebook fa-lg"></i></a>
+                    <a href="https://www.facebook.com"><i class="fab fa-facebook fa-lg"></i></a>
+                    <a href="https://www.facebook.com"><i class="fab fa-facebook fa-lg"></i></a>
+                    <a href="https://www.facebook.com"><i class="fab fa-facebook fa-lg"></i></a>
+                        
                     <a href="https://www.facebook.com"><i class="fab fa-facebook fa-lg"></i></a>
                     <a href="https://www.google.com"><i class="fab fa-google-plus fa-lg"></i></a>
                     <a href="https://www.twitter.com"><i class="fab fa-twitter fa-lg"></i></a>
                 </div>
-<<<<<<< HEAD
             </div>
-=======
-            </div><br/>
-
-
->>>>>>> parent of 8b61fbf... Created index.js
             <div id="navbar">
-            <div id="back">
-
-            </div>
+            <div id="back"></div>
             <nav>
             <div id="inner">
                 <ul class="list">
@@ -130,9 +102,6 @@ function scrollFunction() {
         </div>
         </nav>
         </div>
-
-
-
             
             <div class="carousel">
                 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
@@ -167,10 +136,5 @@ function scrollFunction() {
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<<<<<<< HEAD
-        <script src="jq.js"></script>
-        <script src="index.js"></script>
-=======
->>>>>>> parent of 8b61fbf... Created index.js
     </body>
 </html>
